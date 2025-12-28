023fc943e2f80822bf823d1c6546a0b701bbb94d
CIBI VISHNU A C <cibikomberi@gmail.com>
2025-12-28 12:33:31 +0530

    Fix formatting in deserializer_util.rs

--- src/util/deserializer_util.rs
+++ src/util/deserializer_util.rs
@@ -1,5 +1,5 @@
 use serde::Deserialize;
-
+ 
 pub fn trim<'de, D>(deserializer: D) -> Result<String, D::Error> where D: serde::Deserializer<'de> {
     let s: String = Deserialize::deserialize(deserializer)?;
     Ok(s.trim().to_string())
